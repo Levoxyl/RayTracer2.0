3c596a87c983769a094f0b1b6ea1706bbc69224f
Levoxyl <hereuser47@gmail.com>
2026-06-24 16:33:45 +0300

    refaactor: Change camera default position

--- include/camera.h
+++ include/camera.h
@@ -9,10 +9,10 @@ struct Camera {
     float fov;
     float aspectRatio;
     
-    Camera() : position(-1.0f, -0.5f, 0.0f),
-                lookAt(1.0f, -0.5f, 0.0f),
+    Camera() : position(0.0f, 1.5f, 4.0f),
+                lookAt(0.0f, 0.0f, 0.0f),
                 up(0.0f, 1.0f, 0.0f),
-                fov(45),
+                fov(45.0f),
                 aspectRatio(1.33f) {}
     
     Ray generateRay(float u, float v) const;
