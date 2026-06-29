0b6ae6047feb9894e6f5ea2f0b83b37c5ac9b2da
Levoxyl <hereuser47@gmail.com>
2026-06-29 21:16:34 +0300

    feat: unit sync between cpp and py by removing hardocded values and passing the vars + changing the position to match yawn and pitch trignometry calcs

--- core/camera.cpp
+++ core/camera.cpp
@@ -8,6 +8,7 @@ void Camera::setProjection(const Vector3& pos, const Vector3& target) {
     lookAt.z = target.z;
 
     const float degToRad = 3.14159265f / 180.0f;
+
     float yaw = pos.x * degToRad;
     float pitch = pos.y * degToRad;
 
@@ -24,30 +25,25 @@ void Camera::setProjection(const Vector3& pos, const Vector3& target) {
     position.x = lookAt.x + offsetX;
     position.y = lookAt.y + offsetY;
     position.z = lookAt.z + offsetZ;
-        
-
-    //Vector3 baseTarget(0.f, 0.f, 0.f);
-    //Vector3 basePos(0.f, 1.5f, 4.f);
-
-    //Vector3 fwd = Vector3::normalize(baseTarget - basePos);
-    //Vector3 right = Vector3::normalize(Vector3::cross(fwd, up));
-    //Vector3 trueUp = Vector3::cross(right, fwd);
-
-    //position = basePos + (right * pos.x) + (trueUp * pos.y) + (fwd * (pos.z - 4.f));
-    //
 }
 
 Ray Camera::generateRay(float u, float v) const {
-    // Calculate camera basis vectors
+
     Vector3 w = Vector3::normalize(position - lookAt);
-    Vector3 u_vec = Vector3::normalize(Vector3::cross(up, w));
+    Vector3 localUp = up;
+
+    if (std::abs(w.y) > 0.95f) {
+        localUp = Vector3(.0f, .0f, 1.0f);
+    }
+
+    Vector3 u_vec = Vector3::normalize(Vector3::cross(localUp, w));
     Vector3 v_vec = Vector3::cross(w, u_vec);
     
-    // Calculate viewport dimensions
+    // viewport dimensions
     float halfHeight = tan(fov * 0.5f * (3.14159265f / 180.0f));
     float halfWidth = aspectRatio * halfHeight;
     
-    // Calculate ray direction
+    // ray direction
     Vector3 direction = u_vec * ((u * 2.0f - 1.0f) * halfWidth) +
                         v_vec * ((v * 2.0f - 1.0f) * halfHeight) -
                         w;
--- include/camera.h
+++ include/camera.h
@@ -7,9 +7,9 @@ struct Camera {
     Vector3 lookAt;
     Vector3 up;
     float fov;
-    float aspectRatio;
+    float aspectRatio = 1.0f;
     
-    Camera() : position(0.0f, 1.5f, 4.0f),
+    Camera() : position(0.0f, 5.17f, 19.31f), // Evaluated position for (Yaw: 0, Pitch: 15, Radius: 20)
                 lookAt(0.0f, 0.0f, 0.0f),
                 up(0.0f, 1.0f, 0.0f),
                 fov(45.0f) {}
